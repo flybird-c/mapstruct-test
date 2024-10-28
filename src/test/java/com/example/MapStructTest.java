24683e89f7b76c926a0e938429366be62a3a098e
lzp <1245491965@qq.com>
2024-10-28 20:24:56 +0800

    测试

---
+++ src/test/java/com/example/MapStructTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+
+import org.example.Main;
+import org.example.dto.CaseDTO;
+import org.example.dto.Record;
+import org.example.mapstruct.DtoMapper;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Main.class)
+public class MapStructTest {
+    @Autowired
+    DtoMapper dtoMapper;
+    @Test
+    public void testConvert(){
+
+        Record record = new Record();
+        record.setAjly("101");
+        record.setBz("这是备注");
+        record.setAjzt("1");
+        CaseDTO caseDTO = dtoMapper.JzajConvertCaseDTO(record);
+        Assertions.assertEquals("来源A",caseDTO.getSource());
+        Assertions.assertSame(record.getBz(),caseDTO.getRemark());
+        Assertions.assertSame(record.getAjzt(),caseDTO.getStatus());
+    }
+
+}
